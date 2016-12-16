c4d7ca4d11add73eda6a7467fb73336918f76092
Serjay Ilaga <Serjay Ilaga>
2016-12-16 14:17:48 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
 
 var port = Number(process.env.PORT || 3000);
 
 app.listen(port, function () {
   console.log('Listening on ' + port);
 });
+
+app.use(express.static(__dirname + '/Public'));
